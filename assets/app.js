e16ec58c3dca907b5daf38bdd58816817a8fda41
Maxime Vaultier <mxm.vaultier@gmail.com>
2025-12-16 17:19:13 +0100

    [ADD] Password validator & style

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,11 @@
-import './stimulus_bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+import { createApp } from 'vue';
+import PasswordValidator from './components/PasswordValidator.vue';
+import './styles/app.scss';
+import 'bootstrap';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+const passwordValidatorElement = document.getElementById('password-validator-app');
+
+if (passwordValidatorElement) {
+    const app = createApp(PasswordValidator);
+    app.mount('#password-validator-app');
+}
---
+++ assets/components/PasswordValidator.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="password-validator-container">
+    <div class="mb-3">
+        <label for="plainPassword" class="form-label required">Mot de passe</label>
+        <input 
+            type="password" 
+            id="registration_form_plainPassword" 
+            name="registration_form[plainPassword]" 
+            required="required" 
+            class="form-control" 
+            v-model="password"
+            @input="checkPassword"
+        >
+        <div class="password-feedback mt-2">
+            <ul class="list-unstyled mb-0">
+                <li :class="criteria.length ? 'text-success' : 'text-danger'">
+                    <i :class="criteria.length ? 'bi bi-check-circle-fill' : 'bi bi-x-circle-fill'"></i> 8 caractères minimum
+                </li>
+                <li :class="criteria.special ? 'text-success' : 'text-danger'">
+                    <i :class="criteria.special ? 'bi bi-check-circle-fill' : 'bi bi-x-circle-fill'"></i> Au moins un caractère spécial
+                </li>
+                <li :class="criteria.uppercase ? 'text-success' : 'text-danger'">
+                    <i :class="criteria.uppercase ? 'bi bi-check-circle-fill' : 'bi bi-x-circle-fill'"></i> Au moins une majuscule
+                </li>
+                <li :class="criteria.number ? 'text-success' : 'text-danger'">
+                    <i :class="criteria.number ? 'bi bi-check-circle-fill' : 'bi bi-x-circle-fill'"></i> Au moins un chiffre
+                </li>
+            </ul>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      password: '',
+      criteria: {
+        length: false,
+        special: false,
+        uppercase: false,
+        number: false
+      }
+    };
+  },
+  methods: {
+    checkPassword() {
+      this.criteria.length = this.password.length >= 8;
+      this.criteria.special = /[!@#$%^&*(),.?":{}|<>]/.test(this.password);
+      this.criteria.uppercase = /[A-Z]/.test(this.password);
+      this.criteria.number = /[0-9]/.test(this.password);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.password-feedback ul li {
+    font-size: 0.9em;
+    margin-bottom: 0.2rem;
+}
+.bi {
+    margin-right: 5px;
+}
+</style>
